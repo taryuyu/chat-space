a4c168157544fcc13bafda76d7a4469485f60920
taryuyu <tar.imai.tar@gmail.com>
2020-07-12 16:27:57 +0900

    groupeditingfunction commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root "messages#index"
+=======
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only:[:index, :new, :create, :edit, :update]
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
