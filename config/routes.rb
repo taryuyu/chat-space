73b620743f30c4d06158c39e004a4611d537133a
taryuyu <tar.imai.tar@gmail.com>
2020-07-13 20:39:21 +0900

    message-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # devise_for :users
+  
 
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only:[:index, :new, :create, :edit, :update]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only:[:new, :create, :edit, :update] do
+    resources :messages, only:[:index, :create]
+end   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
